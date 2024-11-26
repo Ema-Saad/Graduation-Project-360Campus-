8504e3a6c51b2ff35a2f22e12eb1f2f9b55a13fc
CoderEx24 <coderex24@protonmail.com>
2024-11-26 09:03:50 +0200

    backend: tests: added studs

--- _360campus/main/tests.py
+++ _360campus/main/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.shortcuts import reverse
+from .models import *
 
-# Create your tests here.
+class TestStudentAccessClassroom(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_access_to_registered_course(self):
+        pass
+
+    def test_access_to_nonregistered_course(self):
+        pass
+
+    def test_access_to_nonexistant_course(self):
+        pass
+
+class TestStudentAccessMaterials(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_access_to_existing_material(self):
+        pass
+
+    def test_access_to_nonexisting_material(self):
+        pass
+
+    def test_access_to_existing_material_in_nonregistered_course(self):
+        pass
+
+    def test_access_to_nonexisting_material_in_nonregistered_course(self):
+        pass
+
+    def test_access_to_materials_in_nonexistant_course(self):
+        pass
+
+

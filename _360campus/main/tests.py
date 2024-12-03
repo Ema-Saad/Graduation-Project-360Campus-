7b24ae8b3ab39d6eaf0b835cad41b8c60dbe8d42
CoderEx24 <coderex24@protonmail.com>
2024-12-03 12:27:53 +0200

    backend: tests: some fixes to the tests

--- _360campus/main/tests.py
+++ _360campus/main/tests.py
@@ -6,25 +6,26 @@ from .models import *
 class TestStudentAccessClassroom(TestCase):
 
     def setUp(self):
-        student = Student.objects.create(name='Student 1')
+        student = Student.objects.create(first_name='John', last_name='Doe', email='email@domain.com', person_type='P')
+        student.set_password('test')
 
-        course = Course.objects.create(name='Course 1', description='Desc. 1')
-        Course.objects.create(name='Course 2', description='Desc. 2')
+        course = Course.objects.create(title='Course 1', description='Desc. 1')
+        Course.objects.create(title='Course 2', description='Desc. 2')
 
-        Enrollment.objects.create(student, course)
+        Enrollment.objects.create(student=student, course=course)
 
-        self.client.login(student)
+        self.client.login(email='email@domain.com', password='test')
 
     def test_access_to_registered_course(self):
-        result = self.client.get(reverse('course_view', kwargs={'course_pk': 1}))
+        result = self.client.get(reverse('main:course_view', kwargs={'course_pk': 1}))
         self.assertEqual(200, result.status_code, 'failed to access a registered course')
 
     def test_access_to_nonregistered_course(self):
-        result = self.client.get(reverse('course_view', kwargs={'course_pk': 2}))
+        result = self.client.get(reverse('main:course_view', kwargs={'course_pk': 2}))
         self.assertEqual(401, result.status_code, 'failed to deny access to unregistered course')
 
     def test_access_to_nonexistant_course(self):
-        result = self.client.get(reverse('course_view', kwargs={'course_pk': 100}))
+        result = self.client.get(reverse('main:course_view', kwargs={'course_pk': 100}))
         self.assertEqual(404, result.status_code, 'failed to return error on nonexistant course')
 
 class TestStudentAccessMaterials(TestCase):

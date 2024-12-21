31a2fc14b400d05c0be8daadb0c368fd06525384
CoderEx24 <coderex24@protonmail.com>
2024-12-21 14:59:44 +0200

    backend: main: refactored Person model

--- _360campus/main/models.py
+++ _360campus/main/models.py
@@ -1,26 +1,7 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
-# Person model with Single Table Inheritance
-class UserManager(BaseUserManager):
-    def create_user(self, email, username, password=None, **extra_fields):
-        """Create and return a regular user with an email and password."""
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, username=username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, username, password=None, **extra_fields):
-        """Create and return a superuser with an email, username, and password."""
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(email, username, password, **extra_fields)
-
-class Person(AbstractBaseUser):
+class Person(AbstractUser):
     PERSON_TYPE_CHOICES = [
         ('S', 'Student'),
         ('P', 'Professor'),
@@ -28,23 +9,8 @@ class Person(AbstractBaseUser):
         ('A', 'Admin'),
     ]
 
-
-    username = models.CharField(max_length=30, unique=True)  
-    email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
     department = models.CharField(max_length=50, null=True, blank=True)
     person_type = models.CharField(max_length=1, choices=PERSON_TYPE_CHOICES)
-
-    # Additional fields required for AbstractBaseUser
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    
-    USERNAME_FIELD = 'email'  
-    REQUIRED_FIELDS = ['username', 'person_type', 'department']
-
-    objects = UserManager()
 
     class Meta:
         verbose_name = "Person"
